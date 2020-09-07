620f37cdf0d6553dbd423c94e7a8b31ae002e80d
rodds93 <rodrigodds96@gmail.com>
2020-09-06 23:26:29 -0300

    Atualização projeto PAMII do dia 27/08/2020

---
+++ AppBindingCommands/AppBindingCommands/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppBindingCommands
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            BindingContext = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<string>(this, "Info", async (msg) =>
+                {
+                    await DisplayAlert("Informação", msg, "OK");
+                });
+
+            MessagingCenter.Subscribe<string>(this, "Pergunta", async (msg) =>
+                {
+
+
+                    if (await DisplayAlert("Confirmação",
+                        $"{msg} , confirma limpeza dos dados?", "Yes", "No"))
+                    {
+                        viewModel.CleanFields();
+                        await DisplayAlert("Informação", "Limpeza realizada com sucesso", "Ok");
+                    }
+
+
+                });
+
+            MessagingCenter.Subscribe<string>(this, "Opcoes", async (msg) =>
+            {
+                string result;
+
+                result = await DisplayActionSheet($"{msg}, selecione uma opção: ",
+                    "Cancelar", "Limpar", "Contar Caracteres", "Exibir Saudação");
+
+                if (result != null)
+                {
+                    if (result.Equals("Limpar"))
+                        viewModel.CleanConfirmation();
+
+                    if (result.Equals("Contar Caracteres"))
+                        viewModel.CountCharacters();
+
+                    if (result.Equals("Exibir Saudação"))
+                        viewModel.ShowMessage();
+                }
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<string>(this, "Info");
+            MessagingCenter.Unsubscribe<string>(this, "Pergunta");
+            MessagingCenter.Unsubscribe<string>(this, "Opcoes");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string informacoes = string.Empty;
+
+            if (Application.Current.Properties.ContainsKey("AcaoInicial"))
+                informacoes += Application.Current.Properties["AcaoInicial"];
+
+            if (Application.Current.Properties.ContainsKey("AcaoStart"))
+                informacoes += Application.Current.Properties["AcaoStart"];
+
+            if (Application.Current.Properties.ContainsKey("AcaoSleep"))
+                informacoes += Application.Current.Properties["AcaoSleep"];
+
+            if (Application.Current.Properties.ContainsKey("AcaoResume"))
+                informacoes += Application.Current.Properties["AcaoResume"];
+
+            lblInformacoes.Text = informacoes;
+
+
+        }
+    }
+}
---
+++ AppBindingCommands/AppBindingCommands/MainPageViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppBindingCommands
+{
+    class MainPageViewModel : INotifyPropertyChanged
+
+    {
+        public ICommand ShowMessageCommand { get; }
+        public ICommand CountCommand { get; }
+
+        public ICommand CleanCommand { get; }
+
+        public ICommand OptionCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        string name = string.Empty;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == null)
+                    return;
+
+                name = value;
+                OnPropertyChanged(nameof(name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+        public string DisplayName => $"Nome digitado : {name}";
+
+
+
+        string displayMessage = string.Empty;
+        public string DisplayMessage
+        {
+            get => displayMessage;
+            set
+            {
+
+                if (displayMessage == null)
+                    return;
+
+                displayMessage = value;
+                OnPropertyChanged(nameof(DisplayMessage));
+
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            ShowMessageCommand = new Command(ShowMessage);
+            CountCommand = new Command(CountCharacters);
+            CleanCommand = new Command(CleanConfirmation);
+            OptionCommand = new Command(ShowOptions);
+        }
+        public void ShowMessage()
+        {
+            string dataProperty = Application.Current.Properties["dtAtual"].ToString();
+
+            DisplayMessage = $"Boa noite {Name}. Hoje é {dataProperty}.";
+
+        }
+
+        public void CountCharacters()
+        {
+            string nameLenght = string.Format("Seu nome tem {0} Letras", name.Length);
+            MessagingCenter.Send<string>(nameLenght, "Info");
+        }
+
+        public void CleanConfirmation()
+        {
+            MessagingCenter.Send<string>(Name, "Pergunta");
+        }
+
+        public void CleanFields()
+        {
+            Name = string.Empty;
+            DisplayMessage = string.Empty;
+            OnPropertyChanged(Name);
+            OnPropertyChanged(DisplayMessage);
+        }
+
+        public void ShowOptions()
+        {
+            MessagingCenter.Send <string>(Name, "Opcoes");
+        }
+    }
+}
